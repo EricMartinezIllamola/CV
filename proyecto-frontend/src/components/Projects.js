9d8b4e56b584a4e69967e6291d96bdaa46c1857f
unknown <eric77711@gmail.com>
2023-07-18 12:42:28 +0200

    continue porfolio

--- proyecto-frontend/src/components/Projects.js
+++ proyecto-frontend/src/components/Projects.js
@@ -3,45 +3,57 @@ import React from 'react';
 function Projects() {
     return (
         <section id="Experiencia">
-                {/* <h1>Experiencia</h1> */}
-                <div className="projects_container">
-                    <div className="project_card">
-                        <div className="project_img"></div>
-                        <div className="project_content">
-                            <h2>Project 1</h2>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, maxime!</p>
+            {/* <h1>Experiencia</h1> */}
+            <div className="projects_container container-fluid">
+                <div className='row'>
+                    <div className='col'>
+                        <div className="project_card">
+                            <div className="project_img"></div>
+                            <div className="project_content">
+                                <h2>Project 1</h2>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, maxime!</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="project_card">
-                        <div className="project_img"></div>
-                        <div className="project_content">
-                            <h2>Project 1</h2>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, maxime!</p>
+                    <div className='col'>
+                        <div className="project_card">
+                            <div className="project_img"></div>
+                            <div className="project_content">
+                                <h2>Project 1</h2>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, maxime!</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="project_card">
-                        <div className="project_img"></div>
-                        <div className="project_content">
-                            <h2>Project 1</h2>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, maxime!</p>
+                    <div className='col'>
+                        <div className="project_card">
+                            <div className="project_img"></div>
+                            <div className="project_content">
+                                <h2>Project 1</h2>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, maxime!</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="project_card">
-                        <div className="project_img"></div>
-                        <div className="project_content">
-                            <h2>Project 1</h2>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, maxime!</p>
+                    <div className='col'>
+                        <div className="project_card">
+                            <div className="project_img"></div>
+                            <div className="project_content">
+                                <h2>Project 1</h2>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, maxime!</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="project_card">
-                        <div className="project_img"></div>
-                        <div className="project_content">
-                            <h2>Project 1</h2>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, maxime!</p>
+                    <div className='col'>
+                        <div className="project_card">
+                            <div className="project_img"></div>
+                            <div className="project_content">
+                                <h2>Project 1</h2>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, maxime!</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
     );
 }
 
