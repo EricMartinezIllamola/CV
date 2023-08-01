dbdd3fcef348138d08349b4effc47c2c6290e2b4
unknown <eric77711@gmail.com>
2023-08-02 01:10:34 +0200

    xx

--- proyecto-frontend/src/components/Header.js
+++ proyecto-frontend/src/components/Header.js
@@ -4,6 +4,10 @@ import { useTranslation } from 'react-i18next';
 function Header() {
     const [t, i18n] = useTranslation("index");
 
+    const handleChangeLng = () => {
+        i18n.changeLanguage(lng)
+    }
+
     const [open_menu, setOpen_menu] = React.useState(false);
 
     return (
@@ -16,18 +20,18 @@ function Header() {
                     <ul className="content_dropdown">
                         <li><a href="/Project_1">{t("header.a4_1")}</a></li>
                         <li><a href="/Project_2">{t("header.a4_2")}</a></li>
-                        <li><a href="#">PROJECT 3</a></li>
+                        {/* <li><a href="#">PROJECT 3</a></li>
                         <li><a href="#">PROJECT 4</a></li>
-                        <li><a href="#">PROJECT 5</a></li>
+                        <li><a href="#">PROJECT 5</a></li> */}
                     </ul>
                 </li>
                 <li><a href="#SoftSkills">{t("header.a5")}</a></li>
                 <li><a href="#Contactar">{t("header.a6")}</a></li>
-                <li className="dropdown last_li"><a className='idioma' href="#Objetivos">{t("header.a7")} <span className="material-symbols-outlined">expand_more</span></a>
+                <li className="dropdown last_li"><button className='idioma'>{t("header.a7")} <span className="material-symbols-outlined">expand_more</span></button>
                     <ul className="content_dropdown">
-                        <li><a href="#Objetivos">{t("header.a7_1")}</a></li>
-                        <li><a href="#Objetivos">{t("header.a7_2")}</a></li>
-                        <li><a href="#Objetivos">{t("header.a7_3")}</a></li>
+                        <li><button onClick={()=>handleChangeLng("ca")}>{t("header.a7_1")}</button></li>
+                        <li><button onClick={()=>handleChangeLng("es")}>{t("header.a7_2")}</button></li>
+                        <li><button onClick={()=>handleChangeLng("en")}>{t("header.a7_3")}</button></li>
                     </ul>
                 </li>
             </ul>
